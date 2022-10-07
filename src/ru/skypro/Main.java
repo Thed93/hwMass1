a9249a4dbbc67d89a0f41f8ddfa9eedf41183c2d
Evgeniy Zinchenko <JjjackVat@gmail.com>
2022-10-07 08:31:41 +0300

    1 tusk

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,11 @@
+package ru.skypro;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tusk 1" + "\n");
+        int[] mass1 = new int[]{1,2,3};
+        double[] mass2 = {1.57, 7.654, 9.986};
+        String[] mass3 = {"a", "b", "c"};
+
+    }
+}
